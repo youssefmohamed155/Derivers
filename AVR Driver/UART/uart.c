77c1a8d11f30025edddda5b9c1e318fdb0a8e21a
youssefmohamed <125400920+youssefmohamed155@users.noreply.github.com>
2024-03-16 00:05:47 +0200

    uart.c

--- AVR Driver/UART/uart.c
+++ AVR Driver/UART/uart.c
@@ -6,7 +6,7 @@
  *
  * Description: Source file for the UART AVR driver
  *
- * Author: Mohamed Tarek
+ * Author: Youssef Mohamed
  *
  *******************************************************************************/
 
